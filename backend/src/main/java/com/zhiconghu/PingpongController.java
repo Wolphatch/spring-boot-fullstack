957c7e2ebacd2f638775ef229f39105892371633
Zhicong Hu <zac.hu3@auspost.com.au>
2023-05-16 16:15:38 +1000

    CD Workflow with new controller

--- backend/src/main/java/com/zhiconghu/PingpongController.java
+++ backend/src/main/java/com/zhiconghu/PingpongController.java
@@ -1,2 +1,14 @@
-package com.zhiconghu;public class PingpongController {
+package com.zhiconghu;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingpongController {
+    record Pingpong(String result){}
+
+    @GetMapping("/ping")
+    public Pingpong getPingpng(){
+        return new Pingpong("Pong");
+    }
 }
